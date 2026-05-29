c374178842bfc6423a8b20633c176242e6ea6c13
soro bedio <sorobedio@soros-MacBook-Pro.local>
2026-05-29 18:38:18 +0900

    updating

--- testcode.py
+++ testcode.py
@@ -5,4 +5,6 @@ from pprint import pprint
 if __name__ == "__main__":
 
 
-    pprint(torch.hub.list("chenyaofo/pytorch-cifar-models", force_reload=True))
+    # pprint(torch.hub.list("chenyaofo/pytorch-cifar-models", force_reload=True))
+    olmo = AutoModelForCausalLM.from_pretrained("allenai/OLMo-2-0425-1B")
+    print(olmo)
