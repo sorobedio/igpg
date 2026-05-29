fffd0d0f7e71bbea2482df6ba3de51d8f9b093c1
soro bedio <sorobedio@soros-MacBook-Pro.local>
2026-05-29 18:43:07 +0900

    updating

--- testcode.py
+++ testcode.py
@@ -8,3 +8,12 @@ if __name__ == "__main__":
     # pprint(torch.hub.list("chenyaofo/pytorch-cifar-models", force_reload=True))
     olmo = AutoModelForCausalLM.from_pretrained("allenai/OLMo-2-0425-1B")
     print(olmo)
+    std = model.state_dict()
+    ws =[]
+    for key in std:
+        vas = std[key].reshape(1, -1).cpu()
+        ws.append(vas)
+        print(f'{key}:---shape--{vas.shape}--mean--{vas.mean()}--std--{vas.std()}')
+    ws = torch.cat(ws, dim=-1)
+    print(f'all params shape:{ws.shape}==mean:{ws.mean()}==std:{ws.std()}')
+    # print(ws)
