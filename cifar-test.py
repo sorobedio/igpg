5b54b48f5738586a777e6b9c8ec03b22c221761d
soro bedio <sorobedio@soros-MacBook-Pro.local>
2026-05-29 17:11:41 +0900

    updating

--- cifar-test.py
+++ cifar-test.py
@@ -1,3 +1,10 @@
 import torch
-model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_repvgg_a0", pretrained=True)
-print(model)
+
+if __name__ == "__main__":
+    model_list=[ 'cifar10_repvgg_a0',
+ 'cifar10_repvgg_a1',
+ 'cifar10_repvgg_a2',]
+    for model_name in model_list:
+        model = torch.hub.load("chenyaofo/pytorch-cifar-models", model_name, pretrained=True)
+        print(model)
+        print('------------------------------------------------------')
